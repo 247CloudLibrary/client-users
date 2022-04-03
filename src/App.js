36d801d7f30f9c44ee5dd6328667a2b0713d61f3
iinsue <hinsu93@naver.com>
2022-04-04 07:09:20 +0900

    [Auth] 유저 프로필 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import UserLoginPage from "./pages/UserLoginPage";
+import UserProfilePage from "./pages/UserProfilePage";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<UserLoginPage />} />
+      <Route path="/profile" element={<UserProfilePage />} />
     </Routes>
   );
 }
---
+++ src/components/auth/UserProfileForm.js
@@ -0,0 +1,9 @@
+const UserProfileForm = () => {
+    return(
+        <div>
+            <h1>User Profile Form</h1>
+        </div>
+    );
+};
+
+export default UserProfileForm;
