b2be4d950f6834a8e9f56230729c96fce00ff9d9
iinsue <hinsu93@naver.com>
2022-04-03 09:56:29 +0900

    [Auth]User 회원가입 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import UserLoginPage from "./pages/UserLoginPage";
+import UserSignUpPage from "./pages/UserSignUpPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<UserLoginPage />} />
+      <Route path="/signUp" element={<UserSignUpPage />} />
     </Routes>
   );
 }
---
+++ src/pages/UserSignUpPage.js
@@ -0,0 +1,11 @@
+import UserSignUpForm from "../components/auth/UserSignUpForm";
+
+const UserSignUpPage = () => {
+  return (
+    <div>
+      <UserSignUpForm />
+    </div>
+  );
+};
+
+export default UserSignUpPage;
