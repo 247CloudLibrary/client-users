529d30bf71814b914a4b3f37a766845c4a052cbd
KimHanWhee <84313936+KimHanWhee@users.noreply.github.com>
2022-04-04 18:37:36 +0900

    lib/api/boardNotice.js 에 API 함수작성

---
+++ src/lib/api/board.js
@@ -0,0 +1,23 @@
+import qs from "qs";
+import client from "./client";
+
+//게시판 목록 조회
+export const listNoticeBoards = ({
+  boardId,
+  title,
+  createdAt,
+  updatedAt,
+  readCounts,
+}) => {
+  const queryString = qs.stringify({
+    boardId,
+    title,
+    createdAt,
+    updatedAt,
+    readCounts,
+  });
+  return client.get(`/boards?${queryString}`);
+};
+
+//게시글 상세 조회
+export const readNoticeBoard = (boardId) => client.get(`/boards/${boardId}`);
