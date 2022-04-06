7f9ebb337f07b0931765cbab59805b1db3fddeba
KimHanWhee <84313936+KimHanWhee@users.noreply.github.com>
2022-04-06 16:07:01 +0900

    boardInformation.js 수정

--- src/lib/api/boardInformation.js
+++ src/lib/api/boardInformation.js
@@ -1,27 +1,7 @@
-import qs from "qs";
 import client from "./client";
 
 //게시판 목록 조회
-export const listInfoBoards = ({
-  type,
-  boardId,
-  title,
-  adminName,
-  createdAt,
-  updatedAt,
-  readCounts,
-}) => {
-  const queryString = qs.stringify({
-    type,
-    boardId,
-    title,
-    adminName,
-    createdAt,
-    updatedAt,
-    readCounts,
-  });
-  return client.get(`/boards?${queryString}`);
-};
+export const listInfoBoard = () => client.get("/boards");
 
 //게시글 상세 조회
-export const readInfoBoard = (boardId) => client.get(`/boards/${boardId}`);
+export const readInfoBoard = () => client.get(`/boards/${boardId}`);
