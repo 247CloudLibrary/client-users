e65c62652f499191ebde2ca5ddf6186db20d87ca
KimHanWhee <84313936+KimHanWhee@users.noreply.github.com>
2022-04-06 16:12:12 +0900

    libraries.js 수정

--- src/lib/api/libraries.js
+++ src/lib/api/libraries.js
@@ -1,14 +1,4 @@
-import qs from "qs";
 import client from "./client";
 
 //도서관 찾기
-export const listLibraries = ({ libraryId, name, address, email, tel }) => {
-  const queryString = qs.stringify({
-    libraryId,
-    name,
-    address,
-    email,
-    tel,
-  });
-  return client.get(`/libraries?${queryString}`);
-};
+export const listLibraries = () => client.get("/libraries");
