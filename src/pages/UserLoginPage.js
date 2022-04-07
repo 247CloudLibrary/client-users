75a7f97076b0943959e8d9072b6b8b5792df3aa8
iinsue <hinsu93@naver.com>
2022-04-07 11:01:14 +0900

    라우트 테스트용 코드추가2

--- src/pages/UserLoginPage.js
+++ src/pages/UserLoginPage.js
@@ -1,9 +1,13 @@
 import UserLoginForm from "../components/auth/UserLoginForm";
+import { Link } from "react-router-dom";
 
 const UserLoginPage = () => {
   return (
     <div>
       <UserLoginForm />
+      <Link to="/profile">
+        <h2>Profile</h2>
+      </Link>
     </div>
   );
 };
