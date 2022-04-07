188799490912219ca580327651562cc3bbb27248
iinsue <hinsu93@naver.com>
2022-04-07 10:27:15 +0900

    라우트 테스트용 코드추가

---
+++ src/components/auth/UserLoginForm.js
@@ -0,0 +1,9 @@
+const UserLoginForm = () => {
+  return (
+    <div>
+      <h1>User Login Form</h1>
+    </div>
+  );
+};
+
+export default UserLoginForm;
---
+++ src/pages/UserLoginPage.js
@@ -0,0 +1,11 @@
+import UserLoginForm from "../components/auth/UserLoginForm";
+
+const UserLoginPage = () => {
+  return (
+    <div>
+      <UserLoginForm />
+    </div>
+  );
+};
+
+export default UserLoginPage;
