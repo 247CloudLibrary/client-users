67217861cbb4ab4f38d150dc81848ade82e81203
iinsue <hinsu93@naver.com>
2022-04-22 17:24:20 +0900

    홈페이지 네비게이션 UI 작성

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { BiSearch } from "react-icons/bi";
+import SearchBar from "./home/SearchBar";
+import Navigations from "./home/Navigations";
+import Feature from "./home/Feature";
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -30,32 +32,12 @@ const HomePage = () => {
       </header>
 
       <main>
-        <div className="main">
-          <form className="home__search-bar">
-            <input name="search-home" type="text" autoComplete="off" required />
-            <label htmlFor="search-home" className="label-name">
-              <span className="content-name">Book Title</span>
-            </label>
-            <BiSearch className="icon" />
-          </form>
-        </div>
-        <div className="feature"></div>
-        <div className="board">
-          <Link to={"/guide"}>
-            <button>이용안내</button>
-          </Link>
-          <Link to={"/notices"}>
-            <button>공지사항</button>
-          </Link>
-          <Link to={"/admin-register"}>
-            <button>관리자신청</button>
-          </Link>
-          <Link to={"/libraries/list"}>
-            <button>도서관찾기</button>
-          </Link>
-        </div>
+        <SearchBar />
+        <Navigations />
+        <Feature />
         <div className="featureDetail">FeatureDetail</div>
       </main>
+
       <footer>
         <div>Footer</div>
       </footer>
