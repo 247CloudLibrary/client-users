f39641f41bf9876fcc25b80ffda7f23373aa016b
yongCloude <76439014+yongCloude@users.noreply.github.com>
2022-04-27 01:23:16 +0900

    Revert "[libraries] icon 추가 및 css 수정"

--- src/components/libraries/LibrariesDetail.js
+++ src/components/libraries/LibrariesDetail.js
@@ -1,22 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
-import {
-  IoLibraryOutline,
-  IoCalendarNumberOutline,
-  IoTimeOutline,
-} from "react-icons/io5";
-import { FaSortNumericUpAlt, FaRegCalendarTimes } from "react-icons/fa";
+import { IoLibraryOutline, IoCalendarNumberSharp } from "react-icons/io5";
+import { FaSortNumericUpAlt } from "react-icons/fa";
 import { AiOutlineWarning } from "react-icons/ai";
-import { GrMapLocation } from "react-icons/gr";
-import { BsTelephone } from "react-icons/bs";
-import { HiOutlineMail } from "react-icons/hi";
-import { GiNightSleep } from "react-icons/gi";
 
 const LibrariesDetail = () => {
   const [libraryData, setLibraryData] = useState([]);
   const location = useLocation();
-  const navigate = useNavigate();
+  const navigte = useNavigate();
   const id = location.state.id;
 
   useEffect(() => {
@@ -31,56 +23,33 @@ const LibrariesDetail = () => {
   }, []);
 
   const LibraryDetailArray = [
-    {
-      value: libraryData.address,
-      key: "address",
-      tag: "주소",
-      icon: <GrMapLocation />,
-    },
-    { value: libraryData.tel, key: "tel", tag: "TEL", icon: <BsTelephone /> },
-    {
-      value: libraryData.email,
-      key: "email",
-      tag: "이메일",
-      icon: <HiOutlineMail />,
-    },
-    {
-      value: libraryData.holiday,
-      key: "holiday",
-      tag: "휴관일",
-      icon: <GiNightSleep />,
-    },
-    {
-      value: libraryData.operatingTime,
-      key: "operatingTime",
-      tag: "운영시간",
-      icon: <IoTimeOutline />,
-    },
+    { value: libraryData.address, key: "address", tag: "주소" },
+    { value: libraryData.tel, key: "tel", tag: "TEL" },
+    { value: libraryData.email, key: "email", tag: "이메일" },
+    { value: libraryData.holiday, key: "holiday", tag: "휴관일" },
+    { value: libraryData.operatingTime, key: "operatingTime", tag: "운영시간" },
   ];
   const toLibMain = () => {
-    navigate(-1);
+    navigte(-1);
   };
 
   return (
     <div id="libraries-detail">
       <div className="header"></div>
-      <div className="library-name">{libraryData.name}</div>
       <div className="info-area">
-        <div className="detail-info">도서관 정보</div>
+        <div className="library-name">{libraryData.name}</div>
         <table className="detail-area">
           {LibraryDetailArray.map((lda) => (
-            <thead className="detail-box" key={lda.key}>
+            <thead key={lda.key}>
               <tr className="detail-text">
-                <td className="td-icon"> {lda.icon}</td>
                 <td className="tag">{lda.tag}</td>
-                <td className={lda.key}>{lda.value}</td>
+                <td className="value">{lda.value}</td>
               </tr>
             </thead>
           ))}
         </table>
       </div>
       <div className="rule-area">
-        <div className="detail-info">이용규정</div>
         <div className="up-rule">
           <div className="library-rule">
             <div className="icon-num">
@@ -91,7 +60,7 @@ const LibrariesDetail = () => {
           </div>
           <div className="library-rule">
             <div className="icon-num">
-              <IoCalendarNumberOutline className="icon" />
+              <IoCalendarNumberSharp className="icon" />
               <span className="text">{libraryData.lendingAvailableDays}</span>
             </div>
             <span className="label">대출 가능 일수</span>
@@ -107,7 +76,7 @@ const LibrariesDetail = () => {
           </div>
           <div className="library-rule">
             <div className="icon-num">
-              <FaRegCalendarTimes className="icon" />
+              <IoCalendarNumberSharp className="icon" />
               <span className="text">{libraryData.longtermOverdueDays}</span>
             </div>
             <span className="label">장기 연체 구분 일수</span>
