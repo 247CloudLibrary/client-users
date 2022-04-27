a702a1171f071b73c4e8ad6d1de9466cbaecc96e
KimHanWhee <84313936+KimHanWhee@users.noreply.github.com>
2022-04-27 19:27:42 +0900

    [Libraries] 검색창 css 미세수정

--- src/components/libraries/LibrariesSearchFilter.js
+++ src/components/libraries/LibrariesSearchFilter.js
@@ -6,7 +6,12 @@ const LibrariesSearchFilter = ({ text, onSubmit }) => {
         placeholder="도서관 검색"
         defaultValue={text}
         onChange={onSubmit}
-        style={{ width: "20rem", height: "2.5rem", fontSize: "1.5rem" }}
+        style={{
+          width: "20rem",
+          height: "2.5rem",
+          fontSize: "1.5rem",
+          marginBottom: "5rem",
+        }}
       />
     </div>
   );
