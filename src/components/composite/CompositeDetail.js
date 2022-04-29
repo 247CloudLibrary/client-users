1f64654dc4618c46a9b50f40377b19be0e595db7
JoosangKim <fable0831@gmail.com>
2022-04-29 10:52:38 +0900

    detail 페이지 css

--- src/components/composite/CompositeDetail.js
+++ src/components/composite/CompositeDetail.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import Footer from "../../pages/home/Footer";
 
 const CompositeDetail = () => {
   const [compositeData, setCompositeData] = useState([]);
@@ -48,45 +49,48 @@ const CompositeDetail = () => {
   ];
 
   return (
-    <div id="composite-detail">
-      <div className="detail-box">
-        <div className="title-area">
-          <div className="title-box">
-            <span className="type">{compositeData.bookType}</span>
-            <span className="title">{compositeData.title}</span>
-          </div>
-        </div>
-        <div className="info-area">
-          <div className="info-box">
-            <div className="image-box">
-              <img
-                className="coverImage"
-                src={compositeData.coverImage}
-                alt=""
-              />
+    <>
+      <div id="composite-detail">
+        <div className="detail-box">
+          <div className="title-area">
+            <div className="title-box">
+              <span className="type">{compositeData.bookType}</span>
+              <span className="title">{compositeData.title}</span>
             </div>
-            <table className="text-box">
-              {CompositeDetailArray.map((data) => (
-                <thead className="label" key={data.key}>
-                  <tr>
-                    <td className="label-tag">{data.tag}</td>
-                    <td className="label-value">{data.value}</td>
-                  </tr>
-                </thead>
-              ))}
-            </table>
           </div>
-        </div>
-        <div className="minititle-box">
-          <div className="minititle">상세정보</div>
-        </div>
-        <div className="contents-area">
-          <div className="contents-box">
-            <p>{compositeData.contents}</p>
+          <div className="info-area">
+            <div className="info-box">
+              <div className="image-box">
+                <img
+                  className="coverImage"
+                  src={compositeData.coverImage}
+                  alt=""
+                />
+              </div>
+              <table className="text-box">
+                {CompositeDetailArray.map((data) => (
+                  <thead className="label" key={data.key}>
+                    <tr>
+                      <td className="label-tag">{data.tag}</td>
+                      <td className="label-value">{data.value}</td>
+                    </tr>
+                  </thead>
+                ))}
+              </table>
+            </div>
+          </div>
+          <div className="minititle-box">
+            <div className="minititle">상세정보</div>
+          </div>
+          <div className="contents-area">
+            <div className="contents-box">
+              <p>{compositeData.contents}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 export default CompositeDetail;
