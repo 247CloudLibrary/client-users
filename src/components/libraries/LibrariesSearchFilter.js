6541f6ea45b7785dec593d3e9bccd39d4a9c6d55
KimHanWhee <84313936+KimHanWhee@users.noreply.github.com>
2022-05-02 14:15:01 +0900

    전체적인 css 수정

--- src/components/libraries/LibrariesSearchFilter.js
+++ src/components/libraries/LibrariesSearchFilter.js
@@ -1,16 +1,18 @@
+import { FaSearch } from "react-icons/fa";
+
 const LibrariesSearchFilter = ({ text, onSubmit }) => {
   return (
-    <div>
+    <div className="search-bar">
+      <FaSearch className="icon" />
       <input
         type="search"
         placeholder="도서관 검색"
         defaultValue={text}
         onChange={onSubmit}
         style={{
-          width: "20rem",
+          width: "25rem",
           height: "2.5rem",
           fontSize: "1.5rem",
-          marginBottom: "5rem",
         }}
       />
     </div>
